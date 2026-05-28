32dd8580453c5dc76c0ae2f46e579543004898c6
chuaphutho85-ops <chuaphutho85@gmail.com>
2026-05-28 15:50:59 +0700

    Mô tả tính năng vừa sửa

--- script.js
+++ script.js
@@ -1,44 +1,4 @@
 // =========================
-// BUTTON
-// =========================
-
-const watchBtn =
-    document.getElementById("watch-btn");
-
-
-// =========================
-// CLICK EVENT
-// =========================
-
-watchBtn.addEventListener("click", function () {
-
-    // Đổi text button
-    watchBtn.innerHTML =
-        "✨ Loading Vlog...";
-
-    // Disable button
-    watchBtn.disabled = true;
-
-    // Delay giả lập
-    setTimeout(function () {
-
-        // Hiện thông báo
-        alert(
-            "🎬 Welcome to My First Vlog!"
-        );
-
-        // Reset button
-        watchBtn.innerHTML =
-            '<i class="ri-play-circle-line"></i> Watch Vlog';
-
-        // Enable lại
-        watchBtn.disabled = false;
-
-    }, 1200);
-
-});
-
-// =========================
 // ELEMENTS
 // =========================
 
@@ -62,34 +22,26 @@ let isPlaying = false;
 
 watchBtn.addEventListener("click", function () {
 
-    // Nếu chưa phát
+    // PLAY
     if (!isPlaying) {
 
-        // Play music
         bgMusic.play();
 
-        // Update state
         isPlaying = true;
 
-        // Change button text
         watchBtn.innerHTML =
             '<i class="ri-pause-circle-line"></i> Pause Music';
-
     }
 
-    // Nếu đang phát
+    // PAUSE
     else {
 
-        // Pause music
         bgMusic.pause();
 
-        // Update state
         isPlaying = false;
 
-        // Reset button
         watchBtn.innerHTML =
             '<i class="ri-play-circle-line"></i> Play Music';
-
     }
 
 });
